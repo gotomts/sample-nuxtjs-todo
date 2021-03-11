da8480e2d6dc6b7ce3831fc0f529f99d76274620
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-03-12 01:26:29 +0900

    feat: HTMLの雛形作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,103 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        sample-nuxtjs-todo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <section class="container">
+    <h1>Todoリスト</h1>
+    <div class="add-area">
+      <input type="text" name="add-name" id="add-name" placeholder="タスクを入力してください">
+      <button id="add-button" class="button button-green">追加</button>
     </div>
-  </div>
+    <div class="filter">
+      <button class="button button-gray is-active">全て</button>
+      <button class="button button-gray">作業前</button>
+      <button class="button button-gray">作業中</button>
+      <button class="button button-gray">完了</button>
+    </div>
+    <table class="lists">
+      <thead>
+        <tr>
+          <th>タスク</th>
+          <th>登録日時</th>
+          <th>状態</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>テスト</td>
+          <td>2020-04-30 17:00</td>
+          <td>
+            <button class="button button--yet">作業前</button>
+          </td>
+          <td>
+            <button class="button button--delete">削除</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 640px;
+  margin: 30px auto;
+}
+h1 {
+  margin-bottom: 20px;
   text-align: center;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+input {
+  width: 400px;
+  padding: 10px;
+  border-radius: 5px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+th, td {
+  padding: 10px 20px;
+  border-collapse: collapse;
 }
-
-.links {
-  padding-top: 15px;
+.button {
+  width: 80px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid gray;
+  background: #fff;
+  font-weight: bold;
+}
+.button:hover {
+  cursor: pointer;
+}
+.button.is-active {
+  border-color: #000;
+  background: #000;
+  color: #fff;
+}
+.button-green {
+  border-color: green;
+  color: green;
+}
+.button--delete {
+  border-color: red;
+  color: red;
+}
+.add-area {
+  margin-bottom: 20px;
+}
+.filter {
+  margin-bottom: 20px;
+}
+.lists {
+  margin-bottom: 20px;
+}
+.lists th {
+  border-bottom: 2px solid #000;
+}
+.lists th:first-child {
+  width: 200px;
+}
+.lists td {
+  border-bottom: 1px solid #000;
 }
 </style>
